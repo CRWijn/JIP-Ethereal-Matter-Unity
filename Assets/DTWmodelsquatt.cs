687638ca0b8331a6aef806d3d7b881ca3c00ff7d
KornelisBB <koen.brouw1@gmail.com>
2023-10-04 14:11:38 +0200

    error weghalen

--- Assets/DTWmodelsquatt.cs
+++ Assets/DTWmodelsquatt.cs
@@ -4,6 +4,7 @@ using System.Collections.Generic; // Import List from System.Collections.Generic
 using System.Linq;
 using System.Text;
 using System.Collections;
+using System.IO;
 using DTW;
 
 public class DTWmodelsquat : MonoBehaviour
@@ -57,87 +58,92 @@ public class DTWmodelsquat : MonoBehaviour
     public void Update()
     {
             //Making the resultValue equal to the input value from myValueA concatenating it with myValueB.
-            Vector3 elleboogpolssnel = heuptraag.position - knietraag.position;
-            Vector3 schouderarmsnel = knietraag.position - enkeltraag.position;
-            Vector3 elleboogpolstraag = heupsnel.position - kniesnel.position;
-            Vector3 schouderarmtraag = kniesnel.position - enkelsnel.position;
+            Vector3 bovenbeentraag = heuptraag.position - knietraag.position;
+            Vector3 onderbeentraag = knietraag.position - enkeltraag.position;
+            Vector3 bovenbeensnel = heupsnel.position - kniesnel.position;
+            Vector3 onderbeensnel = kniesnel.position - enkelsnel.position;
             
-            float anglesnel = Vector3.Angle(schouderarmsnel, elleboogpolssnel);
-            float angletraag = Vector3.Angle(schouderarmtraag, elleboogpolstraag);
+            float anglesnel = Vector3.Angle(onderbeentraag, bovenbeentraag);
+            float angletraag = Vector3.Angle(onderbeensnel, bovenbeensnel);
 
             x[CounterA] = anglesnel;
             y[CounterB] = angletraag;
 
-            if (CounterA == (maxFrameCount-1))
+        if (CounterA == (maxFrameCount - 1))
+        {
+            SimpleDTW simpleDTW = new SimpleDTW(x, y);
+            simpleDTW.computeDTW();
+            double[,] f = simpleDTW.getFMatrix();
+            //Debug.Log (f[15,15]);
+            int i = x.Length;
+            int j = y.Length;
+            double sumX = x[i - 1];
+            double sumY = y[j - 1];
+            int counterX = 1;
+            int counterY = 1;
+            double averageX = 1;
+            double averageY = 1;
+            double AbsDiffXY = 1;
+
+
+            string path = Directory.GetCurrentDirectory();
+            using (StreamWriter sw = new StreamWriter(path + "/output.txt"))
             {
-                SimpleDTW simpleDTW = new SimpleDTW(x, y);
-                simpleDTW.computeDTW();
-                double[,] f = simpleDTW.getFMatrix();
-                //Debug.Log (f[15,15]);
-                int i = x.Length;
-                int j = y.Length;
-                double sumX = x[i-1];
-                double sumY = y[j-1];
-                int counterX = 1;
-                int counterY = 1;
-                double averageX =1;
-                double averageY = 1;
-                double AbsDiffXY =1;
-
-
-                string path = Directory.GetCurrentDirectory();
-                using (StreamWriter sw = new StreamWriter(path + "/output.txt")) {
-                    if (!written) {
-                        Debug.Log("Writing x and y");
-                        foreach (double x_val in x) {
-                            sw.Write(x_val);
-                            sw.Write(", ");
-                        }
-                        sw.Write("\n");
-                        foreach (double y_val in y) {
-                            sw.Write(y_val);
-                            sw.Write(", ");
-                        }
-                        sw.Write("\n");
-                      }
-                    while (i > 0 || j > 0)
+                if (!written)
+                {
+                    Debug.Log("Writing x and y");
+                    foreach (double x_val in x)
                     {
-                        if (f[i - 1, j] <= f[i - 1, j - 1] && f[i - 1, j] <= f[i, j - 1])
-                        {
-                            sumX = sumX + x[i-1];
-	                        counterX++;
-                            i--;
-                        }
-                        else if (f[i, j - 1] <= f[i - 1, j - 1] && f[i, j - 1] <= f[i - 1, j])
-                        {
-                            sumY = sumY + y[j-1];
-	                        counterY++;
-                            j--;
-                        }
-                        else if (f[i - 1, j - 1] <= f[i, j - 1] && f[i - 1, j - 1] <= f[i - 1, j])
-                        {
-                            averageX = sumX/(double)counterX;
-	                        averageY = sumY/(double)counterY;
-	                        AbsDiffXY = Math.Abs(averageX - averageY);
-                            i--;
-                            j--;
-                            sumX = x[i];
-                            sumY = y[j];
-                            counterX = 1;
-                            counterY = 1;
-                        }
-                        //Debug.Log ("i="+i+" ,j="+j);
+                        sw.Write(x_val);
+                        sw.Write(", ");
+                    }
+                    sw.Write("\n");
+                    foreach (double y_val in y)
+                    {
+                        sw.Write(y_val);
+                        sw.Write(", ");
+                    }
+                    sw.Write("\n");
+                }
+                while (i > 0 || j > 0)
+                {
+                    if (f[i - 1, j] <= f[i - 1, j - 1] && f[i - 1, j] <= f[i, j - 1])
+                    {
+                        sumX = sumX + x[i - 1];
+                        counterX++;
+                        i--;
+                    }
+                    else if (f[i, j - 1] <= f[i - 1, j - 1] && f[i, j - 1] <= f[i - 1, j])
+                    {
+                        sumY = sumY + y[j - 1];
+                        counterY++;
+                        j--;
+                    }
+                    else if (f[i - 1, j - 1] <= f[i, j - 1] && f[i - 1, j - 1] <= f[i - 1, j])
+                    {
+                        averageX = sumX / (double)counterX;
+                        averageY = sumY / (double)counterY;
+                        AbsDiffXY = Math.Abs(averageX - averageY);
+                        i--;
+                        j--;
+                        sumX = x[i];
+                        sumY = y[j];
+                        counterX = 1;
+                        counterY = 1;
+                    }
+                    //Debug.Log ("i="+i+" ,j="+j);
 
-                        // Extract data based on the matched frames (i and j)
-                        if (i < x.Length && j < y.Length)
-                        {
-                            double valueFromX = x[i];  // Value from array x at index i
-                            double valueFromY = y[j];  // Value from array y at index j
+                    // Extract data based on the matched frames (i and j)
+                    if (i < x.Length && j < y.Length)
+                    {
+                        double valueFromX = x[i];  // Value from array x at index i
+                        double valueFromY = y[j];  // Value from array y at index j
 
-                            // Do something with the extracted data (e.g., print or store it)
-                            Debug.Log("i =" + i + " : " + valueFromX + " j = " + j + " : " + valueFromY + " Difference = " + Math.Abs(valueFromX - valueFromY));
-                            //Debug.Log(f[1,1]);
-                        if (!written) {
+                        // Do something with the extracted data (e.g., print or store it)
+                        Debug.Log("i =" + i + " : " + valueFromX + " j = " + j + " : " + valueFromY + " Difference = " + Math.Abs(valueFromX - valueFromY));
+                        //Debug.Log(f[1,1]);
+                        if (!written)
+                        {
                             sw.Write("i:");
                             sw.Write(i);
                             sw.Write("j:");
@@ -151,10 +157,11 @@ public class DTWmodelsquat : MonoBehaviour
                 CounterA = 0;
                 CounterB = 0;
             }
-            else
-            {
-                CounterA++;
-                CounterB++;
-            }
+        }
+        else
+        {
+            CounterA++;
+            CounterB++;
+        }
      }
 }
